717c7183e60db8bdd926b7662b0ea7947b59199d
rodgo1314 <36355523+rodgo1314@users.noreply.github.com>
2020-04-24 20:07:53 -0700

    Initial Commit

---
+++ Movies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by Rodrigo Leyva on 4/24/20.
+//  Copyright © 2020 Rodrigo Leyva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
